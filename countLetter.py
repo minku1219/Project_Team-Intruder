ea452e4e43a71b5c3b8d5644c6a27f0f8f6f8310
Mayank <68742521+minku1219@users.noreply.github.com>
2021-07-26 13:24:57 +0530

    Update countLetter.py

--- countLetter.py
+++ countLetter.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python
+
+from pyspark  import  SparkContext
+
+sc=SparkContext("local","App_first")
+
+filename="file:///tmp/data.txt"
+load_file=sc.textFile(filename).cache()
+
+numacount=load_file.filter(lambda s: 'a' in s).count()
+numbcount=load_file.filter(lambda s: 'b' in s).count()
+
+print  ("line with a: ",numacount)
+print  ("line with b: ",numbcount)
 
