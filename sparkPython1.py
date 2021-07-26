9cc109cef41df5bd2772065dea1b18272cf0d0ea
Mayank <68742521+minku1219@users.noreply.github.com>
2021-07-26 13:25:38 +0530

    Update sparkPython1.py

--- sparkPython1.py
+++ sparkPython1.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+
+from pyspark import SparkContext
+
+# connecting sparkcontext with spark master ..
+
+sc=SparkContext("local","app_first")
+# now sc is same as pyspark sc
+firstRDD=sc.textFile('/tmp/data.txt')
+RDD=firstRDD.collect()
+# second method creating data inside python only 
+#data1=["hello","this","is","python","power"]
+# loading data from a list
+
+transform1=sc.parallelize(RDD)
+# now we can do some action 
+
+action1=transform1.count()
+
+#printing
+print("Number of RDD element ",action1)
 
